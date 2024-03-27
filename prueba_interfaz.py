eaa84ced7c81354d39e206649cad019809bf8cdc
jvalladares <jvalladares@teching.com.pe>
2024-03-27 17:41:13 -0500

    desarrollando_interfaz_calculos para reportes en print de un archivo de grafico_ esperando confirmacion de frank

--- prueba_interfaz.py
+++ prueba_interfaz.py
@@ -1,6 +1,13 @@
 import flet as ft
 
 def main(page: ft.Page):
+    page.title = "Probando Interfaz"
+    page.window_width = 1650
+    page.window_height = 1000
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.window_maximizable = False
+    page.window_resizable = False
+    page.window_center()
 
     rail = ft.NavigationRail(
         selected_index=0,
@@ -12,25 +19,26 @@ def main(page: ft.Page):
         group_alignment=-0.9,
         destinations=[
             ft.NavigationRailDestination(
-                icon=ft.icons.CLOUD_UPLOAD_OUTLINED, selected_icon=ft.icons.CLOUD_UPLOAD_ROUNDED, label="Importar Lecturas"
+                icon=ft.icons.CLOUD_UPLOAD_OUTLINED, selected_icon=ft.icons.CLOUD_UPLOAD_ROUNDED, label="Importar Lecturas",
+                padding= 60
             ),
-            ft.Divider(),
             ft.NavigationRailDestination(
                 icon_content=ft.Icon(ft.icons.INSERT_DRIVE_FILE_OUTLINED),
                 selected_icon_content=ft.Icon(ft.icons.INSERT_DRIVE_FILE_ROUNDED),
                 label="Gen. Reportes",
+                padding= 60
             ),
-            ft.Divider(),
             ft.NavigationRailDestination(
                 icon=ft.icons.DELETE_OUTLINED,
                 selected_icon_content=ft.Icon(ft.icons.DELETE_ROUNDED),
                 label_content=ft.Text("Eliminar"),
+                padding= 60
             ),
-            ft.Divider(),
             ft.NavigationRailDestination(
                 icon=ft.icons.DOOR_FRONT_DOOR_OUTLINED,
                 selected_icon_content=ft.Icon(ft.icons.DOOR_FRONT_DOOR_ROUNDED),
                 label_content=ft.Text("Salir"),
+                padding= 60
             ),
         ],
         on_change=lambda e: print("Selected destination:", e.control.selected_index),
@@ -41,7 +49,7 @@ def main(page: ft.Page):
             [
                 rail,
                 ft.VerticalDivider(width=1),
-                ft.Column([ ft.Text("Body!")], alignment=ft.MainAxisAlignment.START, expand=True),
+                ft.Column([ ft.Text("hOLA!")], alignment=ft.MainAxisAlignment.START, expand=True),
             ],
             expand=True,
         )
